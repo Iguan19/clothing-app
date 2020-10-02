d6564e29579cc5c1ca2bb759c88a2ff452318246
Francisco Egea <puertorey@gmail.com>
2020-10-02 16:50:48 +0100

    playing with destructuring inside dropdown component for learning purposes

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -17,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
 	</div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-	cartItems
+const mapStateToProps = state => ({
+	cartItems: state.cart.cartItems
 })
 
 export default connect(mapStateToProps)(CartDropdown);
