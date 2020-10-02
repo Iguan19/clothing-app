a892aaa13bbe6ff01e8a9a7e24fa9d53376c64bb
Francisco Egea <puertorey@gmail.com>
2020-10-02 12:49:01 +0100

    updated shop data with new ids, updated cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -12,9 +12,9 @@ const CollectionPreview = ({ title, items }) => {
 				{
 					items
 						.filter((item, idx) => idx < 4)
-						.map(({ id, ...otherItemProps }) => {
+						.map((item) => {
 							return(
-									<CollectionItem key={id} { ...otherItemProps } />
+									<CollectionItem key={item.id} item={item} />
 							);
 						})
 				}
