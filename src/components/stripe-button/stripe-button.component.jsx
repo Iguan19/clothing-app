e9ef5bd945a5107d81fcca65a789733ac31203fb
Francisco Egea <puertorey@gmail.com>
2020-11-10 15:20:53 +0000

    added stripe button to make test purchases

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey = 'pk_test_51Hle3CKFGbjkzHqwOq0nWXL0wsl1vYf06pmeGsuuKD7mXZJiVDnXwybhw5n6R78C8aHT6DcD1vc8w2WkVbYYYPNu004Yy6HjO8';
+
+	const onToken = token => {
+		console.log(token);
+		alert("Payment successful");
+	}
+
+	return(
+		<StripeCheckout
+			label="Pay Now"
+			name="CRWN Clothing Ltd"
+			billingAddress
+			shippingAddress
+			image='https://sendeyo.com/up/d/f3eb2117da'
+			description={`Your total is $ ${price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+}
+
+export default StripeCheckoutButton;
